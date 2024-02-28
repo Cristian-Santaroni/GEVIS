e500ee3feb41083775545729eefab6e93b7075f9
1800659 <cristian.santaroni16@gmail.com>
2024-02-28 23:29:12 +0100

    add PCA

--- R/R/hello.R
+++ R/R/hello.R
@@ -23,3 +23,57 @@ hello <- function(data) {
 
   return(result)
 }
+
+pca <- function(data,dataC,dataN){
+
+
+  gsmC <- colnames(dataC)
+
+  # Extract GSM headers from dataN
+  gsmN <- colnames(dataN)
+
+
+  # Convert gsmC to a vector
+  gsmC_vector <- unlist(gsmC)
+
+  # Convert gsmN to a vector
+  gsmN_vector <- unlist(gsmN)
+
+  # Remove the last element from gsmC_vector
+  gsmC_vector <- head(gsmC_vector, -1)
+
+  # Remove the last element from gsmN_vector
+  gsmN_vector <- head(gsmN_vector, -1)
+
+  data <- t(data[,c(gsmC_vector, gsmN_vector)])
+
+  groups <- c(rep("case", length(gsmC_vector)), rep("normal", length(gsmN_vector)))
+  pca <- prcomp(data, center = T, scale. = T, retx = T)
+  ################################################
+  # 3. Compute score and score plot
+  # (scores = the coordinates of old data (observations) in the new systems, that are the PCs)
+
+  # pca$x = t(data)*pca$rotation
+  scores <- pca$x
+  # Convert scores to a data frame
+  scores_df <- as.data.frame(scores)
+
+  # Add a column for groups
+  scores_df$Group <- groups
+
+
+  return(scores_df)
+
+
+  #extracted = t(extracted)
+
+
+  #groups <- c(rep("case", length(dataC)), rep("normal", length(dataN)))
+  #pca <- prcomp(extracted, center = T, scale. = T, retx = T)
+  #scores <- pca$x
+
+
+
+
+
+}
